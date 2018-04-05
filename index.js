f723a93129cb56fd6e4aa56e9b429a4dd9065e56
srideviqa7@gmail.com <srideviqa7@gmail.com>
2018-04-05 05:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ function logShout(string){console.log(string.toUpperCase());}
 function logWhisper(string){console.log(string.toLowerCase());}
 function sayHiToGrandma (aWord) {
   var cantUnswer = "I can't hear you!";
+<<<<<<< HEAD
   var yesIndeed ="YES INDEED!";
   var loveYou ="I love you, too.";
   if (aWord.toLowerCase(aWord) === aWord) return cantUnswer;
   if (aWord.toUpperCase(aWord)===aWord) return yesIndeed;
   if (aWord==="I love you, Grandma.") return loveYou;
+=======
+  var yesIndeed=""
+  if (aWord.toLowerCase(aWord) === aWord) return cantUnswer;
+  if (aWord.toUpperCase(aWord)===aWord) return yesIndeed
+>>>>>>> a786dffaf8b88eab6bb91aa474807c27b1bd4085
 }
 
 
